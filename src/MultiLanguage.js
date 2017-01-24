64d69accac5a0f3208f657f275982a922dcd6330
Leonardo Vilarinho <leonardo-i@outlook.com>
2017-01-24 16:58:45 -0200

    update

--- src/MultiLanguage.js
+++ src/MultiLanguage.js
@@ -2,22 +2,23 @@
 
 class MultiLanguage {
 
-  init(path, language) {
+  init(path, language, store) {
+    this._store = store;
     this._path = path;
     this._language = language;
 
-    this.getContent(false, r => sessionStorage.setItem('vue_mlang', JSON.parse(r)));
+    this.getContent(false, r => this._store.state.mlang = JSON.parse(r));
   }
 
   set language(language) {
     if (language != this._language) {
       this._language = language;
-      this.getContent(false, r => sessionStorage.setItem('vue_mlang', JSON.parse(r)));
+      this.getContent(false, r => this._store.state.mlang = JSON.parse(r));
     }
   }
 
   get content() {
-    return sessionStorage.getItem('vue_mlang');
+    return this._store.state.mlang;
   }
 
   getContent(ass, callback) {
@@ -35,9 +36,9 @@ class MultiLanguage {
 
 const multi = new MultiLanguage();
 
-MultiLanguage.install = function (Vue, { path, d_language }) {
+MultiLanguage.install = function (Vue, { path, d_language, store }) {
 
-  multi.init(path, d_language);
+  multi.init(path, d_language, store);
 
   Vue.prototype.changeLanguage = function (newLang) {
     multi.language = newLang;
@@ -45,10 +46,8 @@ MultiLanguage.install = function (Vue, { path, d_language }) {
 
   Vue.prototype.l = function (value) {
     let content = multi.content,
-        params = [],
+        params = value.split('.'),
         param;
-
-    params = value.split('.');
 
     for (param of params) {
       if (content.hasOwnProperty(param)) content = content[param];else return;
