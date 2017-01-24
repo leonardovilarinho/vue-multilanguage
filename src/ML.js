73df6c837e0e5ab0ef05090b9f8dd31479d273d5
Leonardo Vilarinho <leonardo-i@outlook.com>
2017-01-24 16:01:32 -0200

    session storage

---
+++ src/ML.js
@@ -0,0 +1,79 @@
+"use strict";
+
+class MultiLanguage {
+
+  init(path, language) {
+    this._path = path;
+    this._language = language;
+
+    this.getContent(false, r => sessionStorage.setItem('vue_mlang', JSON.parse(r)));
+  }
+
+  set language(language) {
+    if (language != this._language) {
+      this._language = language;
+      this.getContent(false, r => sessionStorage.setItem('vue_mlang', JSON.parse(r)));
+    }
+  }
+
+  get content() {
+    return sessionStorage.getItem('vue_mlang');
+  }
+
+  getContent(ass, callback) {
+    let url = this._path + '/' + this._language + '.json';
+    let rawFile = new XMLHttpRequest();
+
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", url, ass);
+    rawFile.onreadystatechange = function () {
+      if (rawFile.readyState === 4 && rawFile.status == "200") callback(rawFile.responseText);else callback('{}');
+    };
+    rawFile.send(null);
+  }
+}
+
+const multi = new MultiLanguage();
+
+MultiLanguage.install = function (Vue, { path, d_language }) {
+
+  multi.init(path, d_language);
+
+  Vue.prototype.changeLanguage = function (newLang) {
+    multi.language = newLang;
+  };
+
+  Vue.prototype.l = function (value) {
+    let content = multi.content,
+        params = [],
+        param,
+        bind = [],
+        attr;
+
+    if (value.indexOf('|') > -1) {
+      bind = value.split('|');
+      for (attr of bind) {
+        params = attr.split('.');
+        let v = true;
+        for (param of params) {
+          if (content.hasOwnProperty(param)) content = content[param];else {
+            v = false;
+            return;
+          }
+        }
+
+        if (v) return;
+      }
+    } else {
+      params = value.split('.');
+
+      for (param of params) {
+        if (content.hasOwnProperty(param)) content = content[param];else return;
+      }
+    }
+
+    return typeof content == 'string' ? content : '';
+  };
+};
+
+export default MultiLanguage;
--- src/MultiLanguage.js
+++ src/MultiLanguage.js
@@ -1,67 +1,87 @@
-"use strict";
+"use strict"
+ 
+ class MultiLanguage {
+ 
+   init(path, language) {
+     this._path = path
+     this._language = language
+ 
+     this.getContent( false, (r) => sessionStorage.setItem('vue_mlang', JSON.parse(r)) )
+   }
+ 
+   set language(language) {
+     if(language != this._language) {
+       this._language = language
+       this.getContent( false, (r) => sessionStorage.setItem('vue_mlang', JSON.parse(r)) )
+     }
+   }
+ 
+   get content() {
+     return sessionStorage.getItem('vue_mlang')
+   }
+ 
+ 
+   getContent(ass, callback) {
+     let url =  this._path + '/' + this._language + '.json'
+     let rawFile = new XMLHttpRequest()
+ 
+     rawFile.overrideMimeType("application/json")
+     rawFile.open("GET", url, ass)
+     rawFile.onreadystatechange = function() {
+       if (rawFile.readyState === 4 && rawFile.status == "200")
+         callback(rawFile.responseText)
+       else
+         callback('{}')
+     }
+     rawFile.send(null);
+   }
+ }
+ 
+ const multi = new MultiLanguage()
+ 
+ MultiLanguage.install = function(Vue, {path, d_language}){
+ 
+   multi.init(path, d_language)
+ 
+   Vue.prototype.changeLanguage = function(newLang) {
+     multi.language = newLang
+   }
+ 
+  Vue.prototype.l = function(value) {
+     let content = multi.content, params = [], param, bind = [], attr
+ 
+    if(value.indexOf('|') > -1) {
+      bind = value.split('|')
+      for (attr of bind) {
+        params = attr.split('.')
+        let v = true;
+        for (param of params) {
+          if(content.hasOwnProperty(param))
+            content = content[param]
+          else {
+            v = false;
+            return;
+          }
+        }
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+        if(v)
+          return;
+      }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+    } else {
+      params = value.split('.')
 
-var Acl = function () {
-  function Acl() {
-    _classCallCheck(this, Acl);
-  }
-
-  _createClass(Acl, [{
-    key: 'init',
-    value: function init(router, permission, store) {
-      this.router = router;
-      this._store = store;
-      this._store.state.acl_current = permission;
+      for (param of params) {
+       if(content.hasOwnProperty(param))
+         content = content[param]
+       else
+         return;
+      }
     }
-  }, {
-    key: 'check',
-    value: function check(permission) {
-      if (Array.isArray(permission)) return permission.indexOf(this._store.state.acl_current) !== -1 ? true : false;else return this._store.state.acl_current == permission;
-    }
-  }, {
-    key: 'active',
-    set: function set(active) {
-      this._store.state.acl_current = active || null;
-    },
-    get: function get() {
-      return this._store.state.acl_current;
-    }
-  }, {
-    key: 'router',
-    set: function set(router) {
-      var _this = this;
-
-      router.beforeEach(function (to, from, next) {
-        var permission = to.meta.permission.indexOf('.') !== -1 ? to.meta.permission.split('.') : to.meta.permission;
-        if (!_this.check(permission)) return false;
-        next();
-      });
-    }
-  }]);
-
-  return Acl;
-}();
-
-var acl = new Acl();
-
-Acl.install = function (Vue, _ref) {
-  var router = _ref.router,
-      d_permission = _ref.d_permission,
-      store = _ref.store;
-
-  acl.init(router, d_permission, store);
-
-  Vue.prototype.can = function (permission) {
-    permission = permission.indexOf('.') !== -1 ? permission.split('.') : permission;
-    return acl.check(permission);
-  };
-
-  Vue.prototype.changeAccess = function (newAccess) {
-    acl.active = newAccess;
-  };
-};
-
-export default Acl;
+     
+    return (typeof content == 'string') ? content : ''
+   }
+ 
+ }
+ 
+ export default MultiLanguage
